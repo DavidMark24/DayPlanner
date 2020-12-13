af801d227a2e341ee98a4bcfebc2d30e86c7188c
David Johnson <david.m.johnson32@gmail.com>
2020-12-12 18:51:50 -0800

    Added function updateColor and save button eventlistner

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function getLocalStorage(key) {
     var value = localStorage.getItem(key);
     if (value) {
-        $("#text${key}").text(value);
+        $(`#text${key}`).text(value);
     }
 }
 
@@ -11,56 +11,55 @@ function getLocalStorage(key) {
 
 $( document ).ready(function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-   
-// Using for loop to cycle through the times on the planner
+
+    // Using for loop to cycle through the times on the planner
 
     for (var i = 9; i < 18; i++) {
     
-        // create a row
+        // Create a row
         var row = $(`<div data-time=${i} id='${i}' class="row">`);
 
-        // create a column
+        // Create a column
         var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
 
-        //create column 2
+        // Create column 2
         var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="What is the plan today?"></textarea>`);        
        
-        //create column 3
+        // Create column 3
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
         
-       
-         // append col to row
-         row.append(col1);
-         row.append(col2);
-         row.append(col3);
- 
-         // last step add rows to container
-         $(".container").append(row);
- 
-// Using local storage to hold the information gathered from the loop
+        // Append col to row
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
 
-         getLocalStorage(i);
-     }
+        // Last step add rows to container
+        $(".container").append(row);
 
-// Creating function to format hours (am and pm)
+        getLocalStorage(i);
+    }
 
-     function formatAMPM(hours) {
-        var amPm = hours >= 12 ? "pm" : "am";
+
+    // Creating function to update colors for the time
+
+    function formatAMPM(hours) {
+        var amPm = hours >= 12 ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12;
         return hours + amPm;
     }
-
 formatAMPM();
+
+// Creating function to update colors for the time
 
 function updateColors(){
         var currentTime = new Date().getHours();
         for (var i = 9; i < 18; i++) { 
-        console.log(currentTime, $("#${i}").data("time"));
-         if ($("#${i}").data("time") == currentTime){
-            $("#text${i}").addClass( "present");
+        console.log(currentTime, $(`#${i}`).data("time"));
+         if ($(`#${i}`).data("time") == currentTime){
+            $(`#text${i}`).addClass( "present");
         } else if (currentTime < $(`#${i}`).data("time")) {
-            $("#text${i}").addClass( "future");
+            $(`#text${i}`).addClass( "future");
         }
     }
 }
@@ -69,10 +68,11 @@ setInterval(function() {
     updateColors();
 }, 1000);
 
+// Creating event listner for save button to hold the information with local storage
+
 var saveBtn = $('.saveBtn');
 saveBtn.on('click', function(){
-    let eventId = $(this).attr('id');
-    let eventText = $(this).parent().siblings().children('.description').val();
+    var eventId = $(this).attr('id');
+    var eventText = $(this).parent().siblings().children('.description').val();
     localStorage.setItem(eventId, eventText);
 });});
-     
