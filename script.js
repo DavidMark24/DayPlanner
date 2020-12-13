96c271ec345abde642524e77ae0be0c92da92218
David Johnson <david.m.johnson32@gmail.com>
2020-12-12 16:36:54 -0800

    Added local storage function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Creating the local storeage for the save feature
 
 function getLocalStorage(key) {
-    let value = localStorage.getItem(key);
+    var value = localStorage.getItem(key);
     if (value) {
-        $(`#text${key}`).text(value);
+        $("#text${key}").text(value);
     }
 }
 
